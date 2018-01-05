fc7cefb34f25a1ef51403e08f9d92e7b4fd41e42
Ryan Richholt <rrichholt@tgen.org>
2018-01-04 21:17:08 -0700

    fixed backend urls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,12 +4,12 @@ import config from "../config"
 export default {
   getProfile: () => {
     console.log('Requesting user profile...')
-    return axios.get(config.url + "/api/profile")
+    return axios.get(config.url + "api/profile")
   },
 
   register: (formData) => {
     console.log('Sending registration')
-    return axios.post(config.url + "/auth/register", formData)
+    return axios.post(config.url + "auth/register", formData)
   }
 
 };
